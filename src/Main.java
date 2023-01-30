df42191a4c7ace8134836321f67afcab022e83fe
정두원 <96522218+D-w-nJ@users.noreply.github.com>
2023-01-30 20:25:44 +0900

    Queue 4

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.lang.reflect.Member;
+import java.util.*;
+
+class Coor{
+    private int num;
+    private int id;
+    private int rst;
+
+    public int getRst() {
+        return rst;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setRst(int rst) {
+        this.rst = rst;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public Coor(int num, int id, int rst) {
+        this.num = num;
+        this.id = id;
+        this.rst = rst;
+    }
+}
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+
+        ArrayList<Coor> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            list.add(new Coor(Integer.parseInt(st.nextToken()),i,0));
         }
 
-        Arrays.sort(arr);
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            result += arr[i] * (n - i);
-        }
+        Comparator<Coor> comp1 = new Comparator<Coor>(){
+            @Override
+            public int compare(Coor o1, Coor o2) {
+                return o1.getNum()-o2.getNum();
+            }
+        };
+        Collections.sort(list,comp1);
 
-        System.out.println(result);
+        int tmp = 0;
+        for (int i = 1; i < N; i++) {
+            if(list.get(i-1).getNum()==list.get(i).getNum())
+                list.get(i).setRst(tmp);
+            else{
+                list.get(i).setRst(++tmp);
+            }
+        }
+        Comparator<Coor> comp2 = new Comparator<Coor>(){
+            @Override
+            public int compare(Coor o1,Coor o2){
+                return o1.getId()-o2.getId();
+            }
+        };
+        Collections.sort(list,comp2);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(list.get(i).getRst()+" ");
+        }
+        System.out.println(sb);
     }
 }
