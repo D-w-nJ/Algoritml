623c9c237ebf875d7f7da89fd3b38e8ce77b245f
D-w-nJ <jdu2121@ajou.ac.kr>
2023-03-23 15:53:37 +0900

    minor

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 class Main {
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<ArrayList<Integer>> list;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[N][N];
-
+        list = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if (i > 0)
-                    arr[i][j] += arr[i - 1][j];
-                if (j > 0)
-                    arr[i][j] += arr[i][j - 1];
-                if (j > 0 && i > 0)
-                    arr[i][j] -= arr[i - 1][j - 1];
+            list.add(new ArrayList<>());
+        }
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String tmp = st.nextToken();
+            String lNode = st.nextToken();
+            if (!lNode.equals("."))
+                list.get(tmp.charAt(0) - 'A').add(lNode.charAt(0) - 'A');
+            String rNode = st.nextToken();
+            if (!rNode.equals("."))
+                list.get(tmp.charAt(0) - 'A').add(rNode.charAt(0) - 'A');
+        }
+
+        dfs1(0);
+        sb.append("\n");
+        dfs3(0);
+        sb.append("\n");
+        dfs2(0);
+        System.out.println(sb);
+    }
+
+    static void dfs1(int node) {
+        sb.append((char) ('A' + node));
+        for (int i : list.get(node)) {
+            dfs1(i);
+        }
+    }
+
+    static void dfs2(int node) {
+        for (int i : list.get(node)) {
+            dfs2(i);
+        }
+        sb.append((char) ('A' + node));
+    }
+
+    static void dfs3(int node) {
+        boolean flag = false;
+        for (int i : list.get(node)) {
+            dfs3(i);
+            if (!flag) {
+                sb.append((char) ('A' + node));
+                flag = true;
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken()) - 1;
-            int y1 = Integer.parseInt(st.nextToken()) - 1;
-            int x2 = Integer.parseInt(st.nextToken()) - 1;
-            int y2 = Integer.parseInt(st.nextToken()) - 1;
-            if (x1 == 0 && y1 == 0)
-                sb.append(arr[x2][y2]).append("\n");
-            else if (x1 == 0)
-                sb.append(arr[x2][y2] - arr[x2][y1 - 1]).append("\n");
-            else if (y1 == 0)
-                sb.append(arr[x2][y2] - arr[x1 - 1][y2]).append("\n");
-            else
-                sb.append(arr[x2][y2] - arr[x2][y1 - 1] - arr[x1 - 1][y2] + arr[x1 - 1][y1 - 1]).append("\n");
+        if (!flag) {
+            sb.append((char) ('A' + node));
         }
-
-        System.out.println(sb);
     }
 }
